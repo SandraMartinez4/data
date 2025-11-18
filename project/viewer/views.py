d607f71a5c57494a52190da6b5c7dce710420e0b
SandraMartinez4 <sandraallendemartinez1456@gmail.com>
2025-11-18 09:27:57 -0600

    data1

--- project/viewer/views.py
+++ project/viewer/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.http import HttpResponse
 import nbformat
 from nbconvert import HTMLExporter
+from nbconvert.preprocessors import TagRemovePreprocessor, Preprocessor
+
 
 def upload_file(request):
     if request.method == 'POST' and request.FILES.get('file'):
@@ -16,13 +18,23 @@ def upload_file(request):
         return render(request,'uploaded.html',{'filename':f.name})
     return render(request,'upload.html')
 
+
 def view_file(request, filename):
     path = os.path.join(settings.MEDIA_ROOT, filename)
-    with open(path, "rb") as f:
-     nb = nbformat.read(f, as_version=4)
 
-    html_exporter = HTMLExporter()
+    with open(path, "r", encoding="utf-8") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    from nbconvert import HTMLExporter
+    from traitlets.config import Config
+    
+    c = Config()
+    c.HTMLExporter.exclude_input = True
+    c.HTMLExporter.exclude_input_prompt = True
+    
+    html_exporter = HTMLExporter(config=c, template_name="classic")
+
     body, _ = html_exporter.from_notebook_node(nb)
+    
+
     return HttpResponse(body)
-
-
